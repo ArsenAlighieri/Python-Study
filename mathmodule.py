e2a021882dc7b0e0d1bbbd384c3acc7a09df53ba
Akif Buğra Karslı <115721177+ArsenAlighieri@users.noreply.github.com>
2024-08-26 20:15:28 +0300

    Update mathmodule.py

--- mathmodule.py
+++ mathmodule.py
@@ -8,3 +8,16 @@ sonuc = math.sqrt(x) # Kök alma
 sonuc2 = math.ceil(y) # Sayıyı yukarı yuvarlama
 sonuc3 = math.floor(y) # Sayıyı aşağı yuvarama
 
+# Dairenin çevresi
+
+r = float(input("Daire yarıçapını giriniz: "))
+pi = math.pi
+C = 2*pi*r
+print(f"Dairenin çevresi = {round(C,2)}")
+
+# Dairenin alanı
+
+r = float(input("Daire yarıçapını giriniz: "))
+pi = math.pi
+A = round(pi*math.pow(r,2),2)
+print(f"Dairenin alanı = {A}")
