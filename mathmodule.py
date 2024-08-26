a08a13af21eb35f60ae4f8c851acd9472a1d6fce
Akif Buğra Karslı <115721177+ArsenAlighieri@users.noreply.github.com>
2024-08-26 20:03:17 +0300

    Create mathmodule.py

---
+++ mathmodule.py
@@ -0,0 +1,10 @@
+import math
+
+x = 9
+y = 10.2
+print(math.pi) #Pi sayısı
+print(math.e) # "e" sayısı
+sonuc = math.sqrt(x) # Kök alma
+sonuc2 = math.ceil(y) # Sayıyı yukarı yuvarlama
+sonuc3 = math.floor(y) # Sayıyı aşağı yuvarama
+
