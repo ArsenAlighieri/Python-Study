8568ea0b6f76b9598c9bf52fd4916f77e68c7522
Akif Buğra Karslı <115721177+ArsenAlighieri@users.noreply.github.com>
2024-08-26 19:59:42 +0300

    Update arithmetic-and-math.py

---
+++ arithmetic-and-math.py
@@ -0,0 +1,26 @@
+# Aritmetik
+
+sayı = 4 # Sayıyı belirleme
+sayı = sayı + 1 # Sayıyı 1 arttırma
+sayı += 1 # Sayıyı 1 arttırma
+sayı = sayı - 1 # Sayıyı 1 azaltma
+sayı -= 1 # Sayıyı 1 azaltma
+sayı = sayı*3 # Sayının 3 katı
+sayı *= 3 # Sayının 3 katı
+sayı = sayı/2 # Sayının yarısı
+sayı /= 2 # Sayının yarısı
+sayı = sayı ** 2 # Sayının 2. üssü
+sayı **= 2 # Sayının 2. üssü
+kalan = sayı % 3 # Sayının bölümünden kalan 
+
+# Matematik Fonksiyonları
+
+x = 3.14
+y = -4
+z = 5
+sonuc = round(x) # Sayıyı en yakın tam sayıya yuvarlama
+sonuc2 = abs(y) # Sayının mutlak değeri
+sonuc3 = pow(2,3) # Sayının üssü (2 üzeri 3)
+sonuc4 = max(x,y,z) # İçlerindeki en büyük sayı
+sonuc5 = min(x,y,z) # İçlerindeki en küçük sayı
+
