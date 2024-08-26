b0eee853b3f08f9edab827b5a480bc94952ae36a
Akif Buğra Karslı <115721177+ArsenAlighieri@users.noreply.github.com>
2024-08-26 20:43:51 +0300

    Update input.py

--- input.py
+++ input.py
@@ -1,14 +1,14 @@
 # input() = Fonksiyon kullanıcıdan veri almak için kullanılır. ALınan veriyi "string" türünde alır.
 
-# name = input("What is your name?: ")
-# age = int(input("How old are you?: "))
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
 
-# print(f"Hello {name}")
-# print("Happy Birthday!")
-# print(f"You're now {age+1} years old!")
+print(f"Hello {name}")
+print("Happy Birthday!")
+print(f"You're now {age+1} years old!")
 
 # Dikdörtgenin Alanı
-# length = float(input("Metre cinsinden uzunluğu giriniz: "))
-# width = float(input("Metre cinsinden genişliği giriniz: "))
-# area = length*width
-# print(f"Dikdörtgenin alanı {area}m²'dir.")
+length = float(input("Metre cinsinden uzunluğu giriniz: "))
+width = float(input("Metre cinsinden genişliği giriniz: "))
+area = length*width
+print(f"Dikdörtgenin alanı {area}m²'dir.")
